89c69b6264335fd4a8aa8ee9f827baa6b869248c
Gabrielhbs1 <gabrielhsilva1237@gmail.com>
2026-05-28 16:35:06 -0300

    28/02 - 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
-import './App.css'
+import './App.css';
+import Header from "../footer/footer.jsx";
+import Footer from "../header/header.jsx";
 
 function App() {
   return (
+    <>
+    <Header />
+
+    <main>
+
+
+    </main>
+
+    <Footer />
     <h1>Teste</h1>
+    </>
   );
 }
 
-export default App
+export default App;
---
+++ src/footer/footer.jsx
@@ -0,0 +1,5 @@
+import './footer/footer.css';
+
+
+
+
